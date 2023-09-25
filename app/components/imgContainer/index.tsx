e4a94846bb24c6eeb8c200798ef0cede036509bd
Alexandra Bunty <buntyalexandra@gmail.com>
2023-09-25 09:13:15 -0400

    added image opacity to hover

--- app/components/imgContainer/index.tsx
+++ app/components/imgContainer/index.tsx
@@ -7,12 +7,15 @@ type Props = {
 
 export default function ImgContainer({ photo }: Props) {
   return (
-    <div key={photo.id} className="h-64 bg-pink-900 relative overflow-hidden">
+    <div
+      key={photo.id}
+      className="h-64 bg-pink-900 relative overflow-hidden group"
+    >
       <Image
         src={photo.src.large}
         alt={photo.alt}
         fill={true}
-        className="object-cover"
+        className="object-cover group-hover:opacity-75"
       />
     </div>
   );
