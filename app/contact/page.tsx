5cfad185734f11b65fe486fc745174af8fe2ad47
Alexandra Bunty <buntyalexandra@gmail.com>
2023-10-06 15:31:39 -0400

    fixed image stretching on contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -7,18 +7,18 @@ const oi = Oi({ weight: "400", subsets: ["latin"] });
 
 export default function Contact() {
   return (
-    <div className="h-full bg-pink-100 p-8">
-      <div className="border-black border-4 h-full p-8 flex flex-col items-center">
+    <div className="h-screen bg-pink-100 p-8">
+      <div className="border-black border-4 h-fit p-8 flex flex-col items-center md:h-fit">
         <div className={oi.className}>
           <h1 className="text-black text-center text-5xl pb-8 tracking-widest">
             DROP A LINE.
           </h1>
         </div>
-        <div className="flex flex-row flex-wrap justify-center gap-5">
+        <div className="flex flex-row flex-wrap justify-center gap-5 md:gap-12">
           <Image
             src={phone}
             alt="photo of an illustrated vintage red circular corded telephone"
-            className="w-56 h-auto"
+            className="w-52 h-36 md:w-64 md:h-48"
           />
           <ContactForm />
         </div>
