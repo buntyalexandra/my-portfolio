a3dee01df53507d04fa9db2c9586c516b007d60c
Alexandra Bunty <buntyalexandra@gmail.com>
2023-11-02 11:10:08 -0400

    added forward and back arrows to single view of cakes and multi media illustrations

--- app/(portfolio)/cakes/[cakeId]/page.tsx
+++ app/(portfolio)/cakes/[cakeId]/page.tsx
@@ -19,15 +19,29 @@ export default async function Cake({ params }: { params: { cakeId: string } }) {
 
   return (
     <div className="h-screen bg-pink-100 flex flex-row flex-wrap gap-6 p-12 justify-start md:justify-center lg:justify-center">
-      <div className="relative w-72 h-96 overflow-hidden group md:w-80 md:h-96 lg:w-96 lg:h-104">
-        <Image
-          src={`/${cakeInfo.img}`}
-          alt={cakeInfo.alt}
-          key={cakeInfo.id}
-          fill={true}
-          className="object-cover"
-          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-        />
+      <div className="flex flex-col gap-4 items-center">
+        <div className="relative w-72 h-96 overflow-hidden group md:w-80 md:h-96 lg:w-96 lg:h-104">
+          <Image
+            src={`/${cakeInfo.img}`}
+            alt={cakeInfo.alt}
+            key={cakeInfo.id}
+            fill={true}
+            className="object-cover"
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          />
+        </div>
+        <div className="flex flex-row gap-2">
+          <Link href={`/cakes/${cakeInfo.id - 1}`}>
+            <button className="bg-black text-white p-1 text-xs hover:text-pink-900">
+              ←
+            </button>
+          </Link>
+          <Link href={`/cakes/${cakeInfo.id + 1}`}>
+            <button className="bg-black text-white p-1 text-xs hover:text-pink-900">
+              →
+            </button>
+          </Link>
+        </div>
       </div>
       <div className="flex flex-col w-48 gap-2">
         <h1 className="text-black">{cakeInfo.name}</h1>
--- app/(portfolio)/multi-media-illustration/[id]/page.tsx
+++ app/(portfolio)/multi-media-illustration/[id]/page.tsx
@@ -27,15 +27,33 @@ export default async function PrintIllustration({
   if (printIllustrationInfo.orientation === "landscape") {
     return (
       <div className="h-screen bg-pink-100 flex flex-row flex-wrap gap-6 p-12 justify-start md:justify-center lg:justify-center">
-        <div className="relative w-96 h-72 overflow-hidden group md:w-96 md:h-80 lg:w-104 lg:h-96">
-          <Image
-            src={`/${printIllustrationInfo.img}`}
-            alt={printIllustrationInfo.alt}
-            key={printIllustrationInfo.id}
-            fill={true}
-            className="object-cover"
-            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-          />
+        <div className="flex flex-col gap-4 items-center">
+          <div className="relative w-96 h-72 overflow-hidden group md:w-96 md:h-80 lg:w-104 lg:h-96">
+            <Image
+              src={`/${printIllustrationInfo.img}`}
+              alt={printIllustrationInfo.alt}
+              key={printIllustrationInfo.id}
+              fill={true}
+              className="object-cover"
+              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+            />
+          </div>
+          <div className="flex flex-row gap-2">
+            <Link
+              href={`/multi-media-illustration/${printIllustrationInfo.id - 1}`}
+            >
+              <button className="bg-black text-white p-1 text-xs hover:text-pink-900">
+                ←
+              </button>
+            </Link>
+            <Link
+              href={`/multi-media-illustration/${printIllustrationInfo.id + 1}`}
+            >
+              <button className="bg-black text-white p-1 text-xs hover:text-pink-900">
+                →
+              </button>
+            </Link>
+          </div>
         </div>
         <div className="flex flex-col w-48 gap-2">
           <h1 className="text-black">{printIllustrationInfo.name}</h1>
@@ -56,15 +74,33 @@ export default async function PrintIllustration({
   if (printIllustrationInfo.orientation !== "landscape") {
     return (
       <div className="h-screen bg-pink-100 flex flex-row flex-wrap gap-6 p-12 justify-start md:justify-center lg:justify-center">
-        <div className="relative w-72 h-96 overflow-hidden group md:w-80 md:h-96 lg:w-96 lg:h-104">
-          <Image
-            src={`/${printIllustrationInfo.img}`}
-            alt={printIllustrationInfo.alt}
-            key={printIllustrationInfo.id}
-            fill={true}
-            className="object-cover"
-            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-          />
+        <div className="flex flex-col gap-4 items-center">
+          <div className="relative w-72 h-96 overflow-hidden group md:w-80 md:h-96 lg:w-96 lg:h-104">
+            <Image
+              src={`/${printIllustrationInfo.img}`}
+              alt={printIllustrationInfo.alt}
+              key={printIllustrationInfo.id}
+              fill={true}
+              className="object-cover"
+              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+            />
+          </div>
+          <div className="flex flex-row gap-2">
+            <Link
+              href={`/multi-media-illustration/${printIllustrationInfo.id - 1}`}
+            >
+              <button className="bg-black text-white p-1 text-xs hover:text-pink-900">
+                ←
+              </button>
+            </Link>
+            <Link
+              href={`/multi-media-illustration/${printIllustrationInfo.id + 1}`}
+            >
+              <button className="bg-black text-white p-1 text-xs hover:text-pink-900">
+                →
+              </button>
+            </Link>
+          </div>
         </div>
         <div className="flex flex-col w-48 gap-2">
           <h1 className="text-black">{printIllustrationInfo.name}</h1>
