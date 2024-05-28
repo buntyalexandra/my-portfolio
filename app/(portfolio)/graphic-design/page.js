02515a4091f72520ac8bd8b45de3af7196bc1b70
Alexandra Bunty <buntyalexandra@gmail.com>
2024-05-28 11:22:49 -0400

    fixed font size and justification on graphic design page

--- app/(portfolio)/graphic-design/page.js
+++ app/(portfolio)/graphic-design/page.js
@@ -9,11 +9,11 @@ export default function GraphicDesign() {
   return (
     <div className="flex flex-col items-center p-16">
       <div className={oi.className}>
-        <h1 className="text-black text-center text-4xl pb-8 tracking-widest">
+        <h1 className="text-black text-center text-2xl lg:text-4xl pb-8 tracking-widest">
           GRAPHIC DESIGN & <br></br> DIGITAL ILLUSTRATION
         </h1>
       </div>
-      <div className="flex flex-row flex-wrap justify-start gap-8">
+      <div className="flex flex-row flex-wrap justify-center gap-8">
         {graphicDesignData.map((artwork) => {
           return (
             <Link href={`/graphic-design/${artwork.id}`} key={artwork.id}>
