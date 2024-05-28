44edecb8b4776fbd17c14f79f08124444d690be5
Alexandra Bunty <buntyalexandra@gmail.com>
2024-05-28 12:18:10 -0400

    fixed spacing and sizing on web dev page mobile

--- app/(portfolio)/multi-media-illustration/page.js
+++ app/(portfolio)/multi-media-illustration/page.js
@@ -7,7 +7,7 @@ const oi = Oi({ weight: "400", subsets: ["latin"] });
 
 export default function Illustration() {
   return (
-    <div className="flex flex-col items-center p-16">
+    <div className="flex flex-col items-center p-6 lg:p-16">
       <div className={oi.className}>
         <h1 className="text-black text-center text-2xl lg:text-4xl pb-8 tracking-widest">
           MULTI-MEDIA <br></br>ILLUSTRATION
